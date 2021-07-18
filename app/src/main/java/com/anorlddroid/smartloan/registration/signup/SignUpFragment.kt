b62639bafb53ee82d0ade6d2a13284c30fa536a7
anorld-droid <mulindipatrice00@gmail.com>
2021-07-18 15:36:32 +0300

    Created the sign up payment fragment.

--- app/src/main/java/com/anorlddroid/smartloan/registration/signup/SignUpFragment.kt
+++ app/src/main/java/com/anorlddroid/smartloan/registration/signup/SignUpFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Observer
 import com.anorlddroid.smartloan.R
+import com.anorlddroid.smartloan.databinding.SignUpFragmentBinding
 
 class SignUpFragment : Fragment() {
 
@@ -14,13 +17,30 @@ class SignUpFragment : Fragment() {
         fun newInstance() = SignUpFragment()
     }
 
+//    private lateinit var binding : SignupFragmentBinding
     private lateinit var viewModel: SignUpViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = SignUpFragmentBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSignUpPaymentFragment.observe(viewLifecycleOwner,
+            Observer<Boolean> { shouldNavigate ->
+                if (shouldNavigate == true) {
+                    val fragmentTransaction : FragmentTransaction? =
+                        activity?.supportFragmentManager?.beginTransaction()
+                    fragmentTransaction?.replace(R.id.fragment_container_view, SignUpPaymentFragment())
+                    fragmentTransaction?.addToBackStack(null)
+                    fragmentTransaction?.commit()
+                    viewModel.onNavigatedToSignUpPaymentFragment()
+                }
+            })
         return inflater.inflate(R.layout.sign_up_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/anorlddroid/smartloan/registration/signup/SignUpViewModel.kt
+++ app/src/main/java/com/anorlddroid/smartloan/registration/signup/SignUpViewModel.kt
@@ -1,7 +1,20 @@
 package com.anorlddroid.smartloan.registration.signup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _navigateToSignUpPaymentFragment = MutableLiveData<Boolean>()
+    val navigateToSignUpPaymentFragment: LiveData<Boolean>
+        get() = _navigateToSignUpPaymentFragment
+
+    fun onFabClicked() {
+            _navigateToSignUpPaymentFragment.value = true
+
+    }
+
+    fun onNavigatedToSignUpPaymentFragment() {
+        _navigateToSignUpPaymentFragment.value = false
+    }
 }
